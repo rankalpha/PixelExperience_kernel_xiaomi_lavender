0a38c50fe0968e0e25bc84962798acf6f9f0a189
rankalpha <52857054+rankalpha@users.noreply.github.com>
2024-03-16 10:31:24 +0800

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -368,6 +368,13 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
+// patch_begin
+#ifdef CONFIG_KSU
+extern bool ksu_input_hook __read_mostly;
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+#endif
+// patch_end
+
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
@@ -375,6 +382,13 @@ static void input_handle_event(struct input_dev *dev,
 
 	disposition = input_get_disposition(dev, type, code, &value);
 
+// patch_begin
+   #ifdef CONFIG_KSU
+	if (unlikely(ksu_input_hook))
+		ksu_handle_input_handle_event(&type, &code, &value);
+   #endif
+// patch_end
+	
 	if ((disposition & INPUT_PASS_TO_DEVICE) && dev->event)
 		dev->event(dev, type, code, value);
 
